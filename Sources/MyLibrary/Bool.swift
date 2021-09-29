75336ad7bf17fbd9a65c17dfd7b7d3386048788d
JAVIER CRUZ SANTIAGO <javier.cruzs@bancoazteca.com>
2021-09-28 23:38:10 -0500

    [UPDATE] - Creating a unique initialize method for Bool extension

--- Sources/MyLibrary/Bool.swift
+++ Sources/MyLibrary/Bool.swift
@@ -7,7 +7,23 @@
 
 //extension Bool {
 public extension Bool {
-    init?(bit: Int) {
+//    init?(bit: Int) {
+//        switch bit {
+//        case 0:
+//            self = false
+//        case 1:
+//            self = true
+//        default:
+//            return nil
+//        }
+//    }
+//
+//    init?(bit: UInt8) {
+//        let int = Int(bit)
+//        self.init(bit: int)
+//    }
+    
+    init?<Integer: BinaryInteger>(bit: Integer) {
         switch bit {
         case 0:
             self = false
@@ -16,10 +32,5 @@ public extension Bool {
         default:
             return nil
         }
-    }
-    
-    init?(bit: UInt8) {
-        let int = Int(bit)
-        self.init(bit: int)
     }
 }
